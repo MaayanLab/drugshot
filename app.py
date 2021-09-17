aa2d7086d1b335c44ff64eb77d7294263282f9aa
ekropiwnicki <eryk.kropiwnicki@icahn.mssm.edu>
2021-09-17 13:02:22 -0400

    updated web app to include support for AutoRIF

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ Session(app)
 
 print("server started")
 
-print("init DrugRIF")
+print("init RIF files")
 
 drugrif = pd.read_csv('https://appyters.maayanlab.cloud/storage/DrugShot/DrugRIF.tsv.gz',
                       sep = '\t',
@@ -35,9 +35,16 @@ drugrif = pd.read_csv('https://appyters.maayanlab.cloud/storage/DrugShot/DrugRIF
 
 drugrif_counts = dict(Counter(drugrif['name']))
 
+autorif = pd.read_csv('https://appyters.maayanlab.cloud/storage/DrugShot/AutoRIF.tsv.gz',
+                      sep = '\t',
+                      usecols = ['name','PMID']).set_index('PMID')
+
+autorif_counts = dict(Counter(autorif['name']))
+
 print("init similarities")
 
 get_s3_file("drugrif_cooccur")
+get_s3_file("autorif_cooccur")
 get_s3_file("L1000_coexpression")
 
 @app.route(base_name+'/')
@@ -51,10 +58,12 @@ def send_template(template):
 @app.route(base_name+'/api/search', methods=["POST", "GET"])
 def search():
     if request.method == 'GET':
-        input_json = {"term": request.args.get("term")}
+        input_json = {"rif": request.args.get("rif"),
+                      "term": request.args.get("term") }
     else:
         input_json = request.get_json()
 
+    rif = input_json["rif"]
     searchterm = input_json["term"]
 
     stime = time.time()
@@ -62,15 +71,19 @@ def search():
     if searchterm not in session:
         query_ncbi(searchterm, api_key, session)
 
-    associated_drug_counts = drugrif.loc[set(drugrif.index).intersection(set(session[searchterm]))]
+    if rif == "drugrif":
+        associated_drug_counts = drugrif.loc[set(drugrif.index).intersection(set(session[searchterm]))]
+        total_counts = drugrif_counts
+    else:
+        associated_drug_counts = autorif.loc[set(autorif.index).intersection(set(session[searchterm]))]
+        total_counts = autorif_counts
 
     drug_counts = dict(Counter(associated_drug_counts.iloc[:,0]))
     drug_count = {}
     return_size = 0
     for (key, value) in drug_counts.items():
-        if value > len(drug_counts)/200 or value/drugrif_counts[key] > 0.07:
-            return_size = return_size+1
-            drug_count[str(key)] = [value, value/drugrif_counts[key]]
+        return_size = return_size+1
+        drug_count[str(key)] = [value, value/total_counts[key]]
 
     response = {'search_term': searchterm,
                 'PubMedID_count': len(session[searchterm]),
@@ -93,7 +106,7 @@ def associate():
     
     stime = time.time()
 
-    association = associate_drugs(similarity, drugs, drugrif_counts)
+    association = associate_drugs(similarity, drugs, autorif_counts)
 
     response = {'association': association, 
         'query_time': time.time()-stime
@@ -104,47 +117,53 @@ def associate():
 @app.route(base_name+'/api/drugpublicationcount', methods=["POST", "GET"])
 def drugpublicationcount():
     if request.method == 'GET':
-        input_json = {"drugs": request.args.get("drugs")}
+        input_json = {"drugs": request.args.get("drugs"), "rif": request.args.get("rif")}
     else:
         input_json = request.get_json()
     
     drugs = input_json["drugs"]
-
+    rif = input_json["rif"]
+    
+    total_counts = drugrif_counts if rif == 'drugrif' else autorif_counts
     drugcounts = []
     for drug in drugs:
-        if drug in drugrif_counts:
+        if drug in total_counts:
             drugcounts.append({"drug": drug, "publications": drugrif_counts[drug]})
 
-    response = {"drugcount": drugcounts}
+    response = {"drugcount": drugcounts, "rif":rif}
     return jsonify(response)
 
 @app.route(base_name+'/api/drugpublications', methods = ["POST","GET"])
 def drugpublications():
     if request.method == 'GET':
-        input_json = {"drug": request.args.get("drug"), "term": request.args.get("term")}
+        input_json = {"drug": request.args.get("drug"),
+                      "rif": request.args.get("rif"),
+                      "term": request.args.get("term")}
     else:
         input_json = request.get_json()
     
     drug = input_json["drug"]
+    rif = input_json["rif"]
     search_term = input_json["term"]
 
     if search_term not in session:
         query_ncbi(search_term, api_key, session)
 
     pmids = session[search_term]
+    rif_file = drugrif if rif == "drugrif" else autorif
     
     try:
-        drugrif_filtered = drugrif[drugrif['name'] == drug]
+        rif_filtered = rif_file[drug['name'] == drug]
 
         # Note: the drugrif dataframe index is PMIDs
-        drug_pmids = drugrif_filtered.index.tolist()
-        drug_term_pmids = drugrif_filtered[drugrif_filtered.index.isin(pmids)].index.tolist()
+        drug_pmids = rif_filtered.index.tolist()
+        drug_term_pmids = rif_filtered[rif_filtered.index.isin(pmids)].index.tolist()
         
         response = {"drug_pmids": drug_pmids,
                     "drug_term_pmids": drug_term_pmids,
                     "search_term": search_term,
                     "drug_pmids_count": len(drug_pmids),
-                    "drug_term_pmids_count": len(drug_term_pmids_count)}
+                    "drug_term_pmids_count": len(drug_term_pmids)}
 
     except Exception:
         print("drug not found")
