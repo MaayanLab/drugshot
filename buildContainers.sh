41497cf287929d1cc48b3c4f6bd268c1db166667
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2024-04-26 12:31:15 -0400

    Update buildContainers.sh

--- buildContainers.sh
+++ buildContainers.sh
@@ -5,6 +5,6 @@ docker rm $(docker ps -q -f status=exited)
 docker kill drugshot
 docker rm drugshot
 
-docker build -f Dockerfile -t maayanlab/drugshot:1.1 .
+docker build --progress=plain -f Dockerfile --platform=linux/amd64 -t maayanlab/drugshot:1.1 .
 
 docker push maayanlab/drugshot:1.1
